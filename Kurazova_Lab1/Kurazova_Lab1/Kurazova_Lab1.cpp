b2be3c76a6a476fd06f6553de382c35a2fa337a3
greenashberry <rayana050605@gmail.com>
2024-09-10 11:49:06 +0300

    Menu and switch
    Функция вывода меню и шаблон для switch-case

--- Kurazova_Lab1/Kurazova_Lab1/Kurazova_Lab1.cpp
+++ Kurazova_Lab1/Kurazova_Lab1/Kurazova_Lab1.cpp
@@ -5,8 +5,8 @@
 
 struct Pipe
 {
-    int kilometer_mark;
-    float length;
+    std::string name;
+    int length;
     int diameter;
     bool maintenance;
 };
@@ -19,8 +19,39 @@ struct Compression_Station
     int effectiveness;
 };
 
+void menu()
+{
+    std::cout << "Menu: \n";
+    std::cout << "1. Add a pipe \n";
+    std::cout << "2. Add a compression station \n";
+    std::cout << "3. View all objects \n";
+    std::cout << "4. Change pipe's maintenance status \n";
+    std::cout << "5. Change a number of active workshops \n";
+    std::cout << "6. Save \n";
+    std::cout << "7. Load \n";
+    std::cout << "0. Exit \n";
+}
+
 int main()
 {
-
+    while(1)
+    {
+        int command;
+        menu();
+        std::cin >> command;
+        switch (command) {
+        case 1: //Добавить трубу
+        case 2: //Добавить КС
+        case 3: //Просмотр всех объектов
+        case 4: //Редактировать трубу
+        case 5: //Редактировать КС
+        case 6: //Сохранить
+        case 7: //Загрузить
+        case 0:
+            return 1;
+        default:
+            "Input error";
+        }
+    }
 }
 
