671d9059538c2d13ced13fbd2a78abb9b14c6609
greenashberry <rayana050605@gmail.com>
2024-09-10 10:45:35 +0300

    Pipe Structure
    Создание структуры Pipe и описание  свойств структуры

---
+++ Kurazova_Lab1/Kurazova_Lab1/Kurazova_Lab1.cpp
@@ -0,0 +1,18 @@
+﻿// Kurazova_Lab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+struct Pipe
+{
+    int kilometer_mark;
+    float length;
+    int diameter;
+    bool maintenance;
+};
+
+int main()
+{
+
+}
+
